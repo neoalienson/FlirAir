16de8319717b4d560c8e9b55f85f885347d1d050
Neo <neo@orz.hk>
2015-11-22 00:19:31 +0800

    init

---
+++ FlirAir/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  FlirAir
+//
+//  Created by Neo on 11/21/15.
+//  Copyright © 2015 padblish. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
